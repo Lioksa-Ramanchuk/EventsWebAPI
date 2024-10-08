2c51b216726c5354b1b0093062ceb3c5ded78d2e
Lioksa Ramanchuk <lioksa.ramanchuk@gmail.com>
2024-10-08 06:57:37 +0300

    Refactor: register validators from assembly instead of registering each one separately.

--- src/Events.Application/Configuration/ApplicationLayerConfiguration.cs
+++ src/Events.Application/Configuration/ApplicationLayerConfiguration.cs
@@ -1,19 +1,8 @@
 ﻿using Events.Application.Configuration.Settings;
 using Events.Application.Interfaces.Services;
-using Events.Application.Models.Account;
-using Events.Application.Models.AccountRole.AccountRole;
-using Events.Application.Models.Event;
-using Events.Application.Models.EventParticipant.EventParticipant;
-using Events.Application.Models.Notification;
-using Events.Application.Models.Participant;
 using Events.Application.Services;
 using Events.Application.Services.Hosted;
-using Events.Application.Validation.Account;
-using Events.Application.Validation.AccountRole;
-using Events.Application.Validation.Event;
-using Events.Application.Validation.EventParticipant;
-using Events.Application.Validation.Notification;
-using Events.Application.Validation.Participant;
+using Events.Application.Validation.Common;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -49,43 +38,9 @@ public static class ApplicationLayerConfiguration
         AppSettings _
     )
     {
+        services.AddValidatorsFromAssemblyContaining(typeof(ValidatorExtensions));
         services.AddFluentValidationAutoValidation();
-        services.AddScoped<
-            IValidator<AccountFilterRequestModel>,
-            AccountFilterRequestModelValidator
-        >();
-        services.AddScoped<
-            IValidator<AccountRoleFilterRequestModel>,
-            AccountRoleFilterRequestModelValidator
-        >();
-        services.AddScoped<
-            IValidator<AccountSignInRequestModel>,
-            AccountSignInRequestModelValidator
-        >();
-        services.AddScoped<
-            IValidator<AccountUpdateRequestModel>,
-            AccountUpdateRequestModelValidator
-        >();
-        services.AddScoped<IValidator<EventAddRequestModel>, EventAddRequestModelValidator>();
-        services.AddScoped<IValidator<EventFilterRequestModel>, EventFilterRequestModelValidator>();
-        services.AddScoped<
-            IValidator<EventParticipantFilterRequestModel>,
-            EventParticipantFilterRequestModelValidator
-        >();
-        services.AddScoped<IValidator<EventSearchRequestModel>, EventSearchRequestModelValidator>();
-        services.AddScoped<IValidator<EventUpdateRequestModel>, EventUpdateRequestModelValidator>();
-        services.AddScoped<
-            IValidator<ParticipantSignUpRequestModel>,
-            ParticipantSignUpRequestModelValidator
-        >();
-        services.AddScoped<
-            IValidator<ParticipantUpdateRequestModel>,
-            ParticipantUpdateRequestModelValidator
-        >();
-        services.AddScoped<
-            IValidator<NotificationSendRequestModel>,
-            NotificationSendRequestModelValidator
-        >();
+        services.AddFluentValidationClientsideAdapters();
 
         return services;
     }
